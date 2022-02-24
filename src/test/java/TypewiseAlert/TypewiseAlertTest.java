9cc8f3d65668ba172c5704c249ad5c8680d5fbb4
Swetha K A <k.a.swetha850@gmail.com>
2022-02-24 20:26:26 +0530

    TestCases Update

--- src/test/java/TypewiseAlert/TypewiseAlertTest.java
+++ src/test/java/TypewiseAlert/TypewiseAlertTest.java
@@ -4,9 +4,6 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import TypewiseAlert.TypewiseAlert.AlertTarget;
-import TypewiseAlert.TypewiseAlert.CoolingType;
-
 public class TypewiseAlertTest {
 
   @Test
@@ -18,17 +15,17 @@ public class TypewiseAlertTest {
 
   @Test
   public void testClassifyTemperatureBreach() {
-    assertTrue(TypewiseAlert.classifyTemperatureBreach(CoolingType.HI_ACTIVE_COOLING,
-        55) == TypewiseAlert.BreachType.TOO_HIGH);
     assertTrue(
-        TypewiseAlert.classifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING, 35) == TypewiseAlert.BreachType.NORMAL);
+        TypewiseAlert.classifyTemperatureBreach(CoolingType.HiActiveCooling, 55) == TypewiseAlert.BreachType.TOO_HIGH);
     assertTrue(
-        TypewiseAlert.classifyTemperatureBreach(CoolingType.PASSIVE_COOLING, 40) == TypewiseAlert.BreachType.TOO_HIGH);
+        TypewiseAlert.classifyTemperatureBreach(CoolingType.MedActiveCooling, 35) == TypewiseAlert.BreachType.NORMAL);
+    assertTrue(
+        TypewiseAlert.classifyTemperatureBreach(CoolingType.PassiveCooling, 40) == TypewiseAlert.BreachType.TOO_HIGH);
   }
 
   @Test
   public void testCheckAndAlert() {
-    TypewiseAlert.checkAndAlert(AlertTarget.TO_CONTROLLER, CoolingType.HI_ACTIVE_COOLING, 35);
-    TypewiseAlert.checkAndAlert(AlertTarget.TO_EMAIL, CoolingType.PASSIVE_COOLING, 55);
+    TypewiseAlert.checkAndAlert(AlertTarget.ControllerAlert, CoolingType.HiActiveCooling, 35);
+    TypewiseAlert.checkAndAlert(AlertTarget.MailAlert, CoolingType.PassiveCooling, 55);
   }
 }
